0f812c6f02b16d90de1955bc542b001b685f4f66
Leah Weisz <leahw@hokla.com>
2023-08-07 16:55:14 +0200

    feat(updateNotionDatabase): skip old pages and add rtft status

--- index.ts
+++ index.ts
@@ -5,10 +5,21 @@ import { getStatusWithChatGPT } from "./utils/getStatusWithChatGPT";
 import { updateNotionPageStatus } from "./utils/updateNotionPageStatus";
 
 
+const oneWeekAgo = new Date();
+oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
 
 async function updateNotionDatabase() {
     const pages = await getPagesFromNotionDataBase()
+
     for (let page of pages) {
+        const pageDate = new Date(page.properties.Date.date.start);
+        if (pageDate < oneWeekAgo) continue;
+
+        if (page.properties.Tries.number === 1) {
+            await updateNotionPageStatus(page.id, "rtft");
+            continue;
+        }
+
         const blocks = await getNotionPageBlocks(page.id)
         const content = extractTextFromBlocks(blocks)
         const status = await getStatusWithChatGPT(content);
